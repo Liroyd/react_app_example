7796fe9f1b0459ed14cc7b8b273a820d9226c979
Liroyd <alex_liroyd@yahoo.com>
2018-08-12 11:03:00 +0300

    Add 2 players x,o

--- src/app/components/Board/board.js
+++ src/app/components/Board/board.js
@@ -6,13 +6,16 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            isXnext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.isXnext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            isXnext: !this.state.isXnext,});
     }
 
     renderSquare(i) {
@@ -25,7 +28,7 @@ class Board extends React.Component {
     }
 
     render () {
-        const status = 'Next player: X';
+        const status = 'Next player: '+ (this.state.isXnext ? "X" : "O");
 
         return (
             <div>
